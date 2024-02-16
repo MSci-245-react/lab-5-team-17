a336207686421a90ddd995f9cd4765bdc8b68730
AdrielDeVera <adrv.eraa@gmail.com>
2024-02-16 12:23:25 -0500

    Stepping Back Up step

--- cypress/integration/listing-restaurants.spec.js
+++ cypress/integration/listing-restaurants.spec.js
@@ -3,13 +3,14 @@ describe('Listing Restaurants', () => {
       const sushiPlace = 'Sushi Place';
       const pizzaPlace = 'Pizza Place';
   
-      cy.intercept('GET', 'https://api.outsidein.dev/mtyNwCuXbA9G7MfdHuM04XhO8jBJZBko/restaurants',
-        {id: 801, name: sushiPlace},
-        {id: 802, name: pizzaPlace},
+      cy.intercept('GET', 'https://api.outsidein.dev/mtyNwCuXbA9G7MfdHuM04XhO8jBJZBko/restaurants',[        {id: 801, name: sushiPlace},
+      {id: 802, name: pizzaPlace},]
       );
   
-      cy.visit('http://localhost:3000');
+      cy.visit('/');
       cy.contains(sushiPlace);
       cy.contains(pizzaPlace);
     });
   });
+
+  //stepping back up
