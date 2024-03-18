ca2cb9c8ae0fc381bb6a98925753402a2cc6fc28
AdrielDeVera <adrv.eraa@gmail.com>
2024-03-18 16:09:58 -0400

    Chp 7 complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+import store from './store';
+import RestaurantScreen from './components/RestaurantScreen';
 
 export default function App() {
-  +  return (
-    +    <div>
-    +      <RestaurantScreen />
-    +    </div>
-    +  );
+  return (
+    <Provider store={store}>
+      <RestaurantScreen />
+    </Provider >
+  );
 }
